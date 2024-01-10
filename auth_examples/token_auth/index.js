d2b85aa4513be11d6daeb1fadb2495859cf8647e
CherpakAndrii <a.cherpak-2026@kpi.ua>
2024-01-10 17:24:08 +0200

    Lab 4 done

--- auth_examples/token_auth/index.js
+++ auth_examples/token_auth/index.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const dotenv = require('dotenv');
 const port = 3000;
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
+
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const JWT_SECRET = 'secret';
+const GrantTypes = {
+    PASSWORD: 'password',
+    AUTHORIZATION_CODE: 'authorization_code',
+    CLIENT_CREDENTIALS: 'client_credentials',
+    IMPLICIT: 'implicit',
+    REFRESH_TOKEN: 'refresh_token',
+    PASSWORD_REALM: 'http://auth0.com/oauth/grant-type/password-realm',
+};
 
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Andrew',
-        password: 'CherpakAndriiIP-01',
-        username: 'Андрій',
-    }
-]
+app.get('/create-user', (req, res) => {
+    return res.sendFile(path.join(__dirname + '/create-user.html'));
+});
 
-app.get('/login', verifyToken, (req, res) => {
+app.get('/', verifyToken, (req, res) => {
     res.json({
-        username: req.user.username,
+        username: req.user,
+        exp: req.exp,
         logout: 'http://localhost:3000/logout'
     });
 })
@@ -34,28 +37,86 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-app.post('/api/login', (req, res) => {
-    const { login, password } = req.body;
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
 
-    const user = users.find((user) => {
-        return user.login === login && user.password === password;
-    });
-
-    if (user) {
-        const token = jwt.sign({ username: user.username, login: user.login }, JWT_SECRET);
-
-        jwt.verify(token, JWT_SECRET, (err, decoded) => {
-            if (err) {
-                console.log('not valid token generated');
+    try {
+        const response = await axios.post(`${process.env.API_URL}oauth/token`, {
+            grant_type: GrantTypes.PASSWORD_REALM,
+            audience: process.env.AUDIENCE_URL,
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            scope: 'offline_access',
+            realm: 'Username-Password-Authentication',
+            username: username,
+            password: password,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
-            else {
-                console.log(token);
+        });
+        res.status(201).json({ access_token: response.data.access_token, refresh_token: response.data.refresh_token, username: username });
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({ error: error.response.data });
+    }
+});
+
+app.post('/api/create', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const response = await axios.post(`${process.env.API_URL}oauth/token`, {
+            grant_type: GrantTypes.CLIENT_CREDENTIALS,
+            audience: process.env.AUDIENCE_URL,
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
         });
 
-        res.json({ token });
-    } else {
-        res.status(401).send('Unauthorized');
+        const response2 = await axios.post(`${process.env.AUDIENCE_URL}users`, {
+            email: email,
+            password: password,
+            connection: 'Username-Password-Authentication'
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${response.data.access_token}`
+            }
+        });
+        res.status(201).json({ message: 'User created' });
+    } catch (error) {
+        res.status(401).json({ error: error.response.data });
+
+    }
+});
+
+app.post('/api/refresh', async (req, res) => {
+    const { refresh_token } = req.body;
+    try {
+        const response = await axios.post(`${process.env.API_URL}oauth/token`, {
+            grant_type: GrantTypes.REFRESH_TOKEN,
+            audience: process.env.AUDIENCE_URL,
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            refresh_token: refresh_token,
+            scope: 'offline_access',
+            realm: 'Username-Password-Authentication',
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+        res.json({ access_token: response.data.access_token});
+    } catch (error) {
+        res.status(401).json({ error: error.response.data });
+
     }
 });
 
@@ -63,22 +124,26 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-function verifyToken(req, res, next) {
-    const token = req.header('Authorization').split(' ')[1];
-
+async function verifyToken(req, res, next) {
+    const token = req.header('Authorization');
     if (!token) {
-        console.log('No token');
         return res.sendFile(path.join(__dirname + '/index.html'));
     }
 
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
-        if (err) {
-            console.log(token);
-            console.log('error');
-            return res.sendFile(path.join(__dirname + '/index.html'));
-        }
+    decoded = jwt.decode(token);
 
-        req.user = decoded;
-        next();
-    });
+    if (!decoded) {
+        return res.sendFile(path.join(__dirname + '/index.html'));
+    }
+    const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+    if (decoded.exp > currentTimestampInSeconds) {
+        const response = await axios.get(`${process.env.AUDIENCE_URL}users/${decoded.sub}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        req.user = response.data.name;
+        req.exp = decoded.exp;
+    }
+    next();
 }
